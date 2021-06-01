0df7f223fac413cae8e469d27437214c8be60ee6
Shivam4819 <shivam4819@gmail.com>
2021-06-01 11:22:21 +0530

    circular linklist

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,126 +2,162 @@ package com.company;
 
 
 import com.Linklist.BasicLinkList;
+import com.Linklist.DoubleLinklist;
 
 import java.util.Scanner;
 
- class DoublyLinkList{
-
-    DoublyLinkList prev;
+class CircularLinklist{
     int data;
-    DoublyLinkList next;
-    DoublyLinkList(int d){
-        prev=null;
-        data=d;
-        next=null;
-    }
+    CircularLinklist next;
 
+    public CircularLinklist(int data) {
+        this.data = data;
+        this.next = null;
+    }
 }
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
 
-       // BasicLinkList list=new BasicLinkList();
+        BasicLinkList list=new BasicLinkList();
        // list.controller();
-        DoublyLinkList d=new DoublyLinkList(10);
-        d.next=new DoublyLinkList(20);
-     //   d.next.prev=d;
-        d.next.next=new DoublyLinkList(30);
-       // d.next.next.prev=d.next;
-        d.next.next.next=new DoublyLinkList(40);
-        d.next.next.next.next=new DoublyLinkList(50);
-        d.next.prev=d;
-        d.next.next.prev=d.next;
-        d.next.next.next.prev=d.next.next;
-        d.next.next.next.next.prev=d.next.next.next;
+
+        DoubleLinklist doubleLinklist=new DoubleLinklist();
+       // doubleLinklist.doubleLinklistController();
+
+        CircularLinklist c=new CircularLinklist(10);
+        c.next=new CircularLinklist(20);
+        c.next.next=new CircularLinklist(30);
+        c.next.next.next=new CircularLinklist(40);
+        c.next.next.next.next=c;
 
         Main m=new Main();
-        m.traverse(d);
-      //  m.backtraverse(d);
-       // m.inserthead(5,d);
-       // m.insertSorted(35,d);
-       // m.removehead(d);
-      //  m.deletenode(30,d);
-        m.copyListReversed(d);
+        m.traverse(c);
+      //  m.insertAtHead(5,c);
+    //    m.insertAtLast(50,c);
+      //  m.searchElement(40,c);
+    //    m.removeFirstNode(c);
+    //    m.removeparticular(40,c);
+       // m.copyReversed(c);
+        m.copyCircular(c);
     }
 
-    public void copyListReversed(DoublyLinkList head){
-        DoublyLinkList temp=head;
-        DoublyLinkList headtwo=null;
-        while (temp!=null){
-            if(headtwo==null){
-                headtwo=new DoublyLinkList(temp.data);
+    public void copyCircular(CircularLinklist head){
+        CircularLinklist temp=head;
+        CircularLinklist headtwo=null;
+        CircularLinklist newnode;
+        CircularLinklist temp2=null;
 
+        while (temp.next!=head){
+            if(headtwo==null){
+                headtwo=new CircularLinklist(temp.data);
+                temp2=headtwo;
             }
             else {
-                DoublyLinkList newnode=new DoublyLinkList(temp.data);
+                newnode=new CircularLinklist(temp.data);
+                temp2.next=newnode;
                 newnode.next=headtwo;
-                headtwo.prev=newnode;
-                headtwo=newnode;
+                temp2=temp2.next;
             }
             temp=temp.next;
         }
-        System.out.println(".................");
+        newnode=new CircularLinklist(temp.data);
+        temp2.next=newnode;
+        newnode.next=headtwo;
+        
         traverse(headtwo);
     }
-    public void deletenode(int x,DoublyLinkList head){
-        DoublyLinkList temp=head;
-        while (temp.next!=null && temp.next.data!=x){
+    public void copyReversed(CircularLinklist head){
+        CircularLinklist temp=head;
+        CircularLinklist newnode;
+        CircularLinklist headtwo=null;
+
+            while (temp.next!=head){
+                if(headtwo==null){
+                    headtwo=new CircularLinklist(temp.data);
+                    //headtwo.next=null;
+                }
+                else {
+                    newnode=new CircularLinklist(temp.data);
+                    newnode.next=headtwo;
+                 //   headtwo.next=newnode;
+                    headtwo=newnode;
+                }
+                temp=temp.next;
+            }
+            newnode=new CircularLinklist(temp.data);
+            newnode.next=headtwo;
+            headtwo=newnode;
+            temp=headtwo;
+       // System.out.println(temp.data);
+            while (temp.next!=null)
+               temp=temp.next;
+             temp.next=headtwo;
+
+            traverse(headtwo);
+
+    }
+    public void removeparticular(int x,CircularLinklist head){
+        CircularLinklist temp=head;
+        while (temp.next.data!=x && temp.next!=head)
             temp=temp.next;
-        }
-        System.out.println(temp.data);
         temp.next=temp.next.next;
-        temp.next.prev=temp;
-        traverse(head);
-      //  backtraverse(head);
-    }
-    public void removehead(DoublyLinkList head){
-        head=head.next;
-        head.prev=null;
         traverse(head);
     }
-    public void insertSorted(int x,DoublyLinkList head){
-        DoublyLinkList temp=head;
-        while (temp.next.data<x && temp.next!=null)
+    public void removeFirstNode(CircularLinklist head){
+        CircularLinklist temp=head;
+        while (temp.next!=head)
             temp=temp.next;
-        System.out.println(temp.data);
-        DoublyLinkList newnode=new DoublyLinkList(x);
-        newnode.next=temp.next;
-        newnode.prev=temp;
-        temp.next=newnode;
-        newnode.next.prev=newnode;
-
+        head=head.next;
+        temp.next=head;
         traverse(head);
+    }
+    public void searchElement(int x,CircularLinklist head){
+        CircularLinklist temp=head;
+        while (temp.next!=head){
+            if(temp.data==x){
+                System.out.println("found");
+                break;
+            }
+            temp=temp.next;
+        }
+        if(temp.data==x){
+            System.out.println("found");
+        }
 
     }
-
-    public void traverse(DoublyLinkList head){
-        while (head!=null){
-            System.out.print(head.data+" ");
-            head=head.next;
+    public void insertAtLast(int x,CircularLinklist head){
+        CircularLinklist temp=head;
+        CircularLinklist newnode= new CircularLinklist(50);
+        while (temp.next!=head){
+            temp=temp.next;
         }
+        temp.next=newnode;
+        newnode.next=head;
+        traverse(head);
+    }
+    public void insertAtHead(int x, CircularLinklist head){
+        CircularLinklist temp=head;
+        CircularLinklist newnode=new CircularLinklist(x);
+       // newnode.next=head;
+        while (temp.next!=head){
+            temp=temp.next;
+        }
+        temp.next=newnode;
+        newnode.next=head;
+        head=newnode;
+
+        traverse(head);
+    }
+    public void traverse(CircularLinklist head){
+        CircularLinklist temp=head;
+        while (temp.next!=head){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+        System.out.print(temp.data);
         System.out.println();
     }
-
-    public void backtraverse(DoublyLinkList head){
-        DoublyLinkList temp=head;
-        while (temp.next!=null)
-            temp=temp.next;
-        System.out.println(temp.data);
-        while (temp!=null){
-            System.out.print(temp.data+" ");
-            temp=temp.prev;
-        }
-    }
-
-    public void inserthead(int x,DoublyLinkList head){
-        DoublyLinkList newnode=new DoublyLinkList(x);
-        newnode.next=head;
-        head.prev=newnode;
-        head=newnode;
-        traverse(head);
-    }
-
-
 }
